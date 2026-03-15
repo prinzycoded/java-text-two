670483d2c9f31527658f7064cfcaaab97844d44b
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-15 20:42:56 +0100

    two added

---
+++ Assignment/two.js
@@ -0,0 +1,5 @@
+let word = "real-madrid";
+console.log("Number of characters:", word.length);
+console.log("First character:", word[0]);
+console.log("Last character:", word[word.length - 1]);
+console.log("Reversed word:", word.split("").reverse().join(""));
