d418947cde6c1d1fa58cf0c19cd212c6423fed0c
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-15 20:17:26 +0100

    number one

---
+++ Assignment/one.js
@@ -0,0 +1,9 @@
+let firstName = "Okoro";
+let lastName = "Prince";
+let age = 26;
+let country = "Nigeria";
+let hobby = "praying";
+
+console.log(`My full name is ${firstName} ${lastName}.`);
+console.log(`I am ${age} years old and I live in ${country}.`);
+console.log(`In my free time, I enjoy ${hobby}.`);
