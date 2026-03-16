28c4ed4eb39eab2b4869db68f68bcf20225a13af
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 10:25:09 +0100

    number ten

---
+++ Assignment/question.ten.js
@@ -0,0 +1,11 @@
+let a = 42;
+let b = 78;
+let c = 55;
+
+if (a >= b && a >= c) {
+  console.log("The largest number is:", a);
+} else if (b >= a && b >= c) {
+  console.log("The largest number is:", b);
+} else {
+  console.log("The largest number is:", c);
+}
