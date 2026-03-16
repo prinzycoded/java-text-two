093dc33578aea393bec1f367fbef73980c2b308b
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-16 10:31:12 +0100

    number eleven

---
+++ Assignment/evelen.js
@@ -0,0 +1,13 @@
+let sentence = "Web.development is fun";
+
+// 1. Number of characters (including spaces)
+console.log("Character count:", sentence.length);
+
+// 2. Sentence in uppercase
+console.log("Uppercase:", sentence.toUpperCase());
+
+// 3. Sentence in lowercase
+console.log("Lowercase:", sentence.toLowerCase());
+
+// 4. Check if the sentence includes "fun"
+console.log("Includes 'fun':", sentence.includes("fun"));
